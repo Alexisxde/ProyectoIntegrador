a9bb10c4b5d0a694eaab3f80356f8e024bad0153
Alexisxde <olivarezalexis749@gmail.com>
2023-09-11 12:11:50 -0300

    edit(admin.py & models.py): Tiraba error al crear una tarea en el panel de administrador 🐛

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from.models import tarea
+from.models import Tarea
 
-admin.site.register(tarea)
-
-# Register your models here.
+admin.site.register(Tarea)
--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-class tarea(models.Model):
+class Tarea(models.Model):
     titulo=models.CharField(max_length=20)
     descripcion=models.CharField(max_length=50)
 
     def __str__(self):
-        return self.titulo, self.descripcion
-
-# Create your models here.
+        return self.titulo + "," + self.descripcion
