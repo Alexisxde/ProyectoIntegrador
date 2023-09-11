d7379b286c356e395aab3258450ef8d1135a2255
Federico Cornaglia <fedesc89@hotmail.com>
2023-09-11 00:51:06 -0300

    new(models.py & admin.py): Agregé funcion y tabla en admin para titulo y descripcion de tarea 🚧

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+class tarea(models.Model):
+    titulo=models.CharField(max_length=20)
+    descripcion=models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.titulo, self.descripcion
+
 # Create your models here.
